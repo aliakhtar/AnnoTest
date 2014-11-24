5a6c04af6d5e586ba304a3cf379d4539e71c63a9
Ali <ali.rac200@gmail.com>
2014-11-24 08:37:09 +0500

    Added abstract AnnoTest for other test classes to extend from.

---
+++ src/main/java/com/github/annoTest/util/AnnoTest.java
@@ -0,0 +1,36 @@
+/*
+ * @author Ali Akhtar
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.github.annoTest.util;
+
+import org.mockito.Mockito;
+
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.Processor;
+
+public abstract class AnnoTest
+{
+    protected final Messager messager;
+    protected final ProcessorWrapper processor;
+    protected final Compiler compiler;
+
+    public AnnoTest(Processor processor, Class... classPathEntries)
+            throws Exception
+    {
+        messager = Mockito.mock(Messager.class);
+        this.processor = new ProcessorWrapper(processor, messager);
+        compiler = new Compiler(classPathEntries);
+    }
+}
